c5330949e684ca9a9aa3fb8ac698c07190c918d7
jmcaliece <jmcaliece@mycorpaccess.com>
2024-12-31 07:55:12 -0800

    Telemtry Proof

---
+++ SK-ObservabilitySolution/SK-Observability/Program.cs
@@ -0,0 +1,124 @@
+﻿#pragma warning disable SKEXP0050
+
+using Microsoft.SemanticKernel;
+using SK_Observability.Settings;
+using SK_Observability.Plugins;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Resources;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+
+namespace SK_Observability
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Write("Observability Proof");
+            // Telemetry setup code goes here
+            var resourceBuilder = ResourceBuilder
+             .CreateDefault()
+             .AddService("TelemetryConsoleQuickstart");
+
+            // Enable model diagnostics with sensitive data.
+            AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+
+            using var traceProvider = Sdk.CreateTracerProviderBuilder()
+                .SetResourceBuilder(resourceBuilder)
+                .AddSource("Microsoft.SemanticKernel*")
+                .AddConsoleExporter()
+                .Build();
+
+            using var meterProvider = Sdk.CreateMeterProviderBuilder()
+                .SetResourceBuilder(resourceBuilder)
+                .AddMeter("Microsoft.SemanticKernel*")
+                .AddConsoleExporter()
+                .Build();
+
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                // Add OpenTelemetry as a logging provider
+                builder.AddOpenTelemetry(options =>
+                {
+                    options.SetResourceBuilder(resourceBuilder);
+                    options.AddConsoleExporter();
+                    // Format log messages. This is default to false.
+                    options.IncludeFormattedMessage = true;
+                    options.IncludeScopes = true;
+                });
+                builder.SetMinimumLevel(LogLevel.Information);
+            });
+
+            var builder = Kernel.CreateBuilder();
+            builder.AddAzureOpenAIChatCompletion(
+                AzureOpenAiSettings.DeploymentName,
+                AzureOpenAiSettings.Endpoint,
+                AzureOpenAiSettings.ApiKey,
+                AzureOpenAiSettings.ModelName);
+
+            var kernel = builder.Build();
+            var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+            // Add a plugin (the LightsPlugin class is defined below)
+            kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+
+            // Enable planning
+            OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+            };
+
+            // Create a history store the conversation
+            var history = new ChatHistory();
+            history.AddUserMessage("First: Are the lights on?");
+
+            // Get the response from the AI
+            var result = await chatCompletionService.GetChatMessageContentAsync(
+               history,
+               executionSettings: openAIPromptExecutionSettings,
+               kernel: kernel);
+
+            // Print the results
+            Write("Assistant > " + result);
+
+            // Add the message from the agent to the chat history
+            history.AddAssistantMessage(result.ToString());
+
+            history.AddUserMessage("Turn on the Porch");
+
+            // Get the response from the AI
+            result = await chatCompletionService.GetChatMessageContentAsync(
+               history,
+               executionSettings: openAIPromptExecutionSettings,
+               kernel: kernel);
+            history.AddAssistantMessage(result.ToString());
+
+            history.AddUserMessage("Last: Are the lights on?");
+
+            // Get the response from the AI
+            result = await chatCompletionService.GetChatMessageContentAsync(
+               history,
+               executionSettings: openAIPromptExecutionSettings,
+               kernel: kernel);
+
+            // Print the results
+            Write("Assistant > " + result);
+
+            Write("Press any key to end");
+            Console.ReadKey();
+        }
+        private static void Write(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+       
+    }
+}
---
+++ SK-ObservabilitySolution/SK-Observability/Settings/AzureOpenAiSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SK_Observability.Settings
+{
+    public static class AzureOpenAiSettings
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static AzureOpenAiSettings()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory) 
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        public static string Endpoint => Configuration["AzureOpenAI:Endpoint"];
+        public static string ApiKey => Configuration["AzureOpenAI:ApiKey"];
+        public static string DeploymentName => Configuration["AzureOpenAI:DeploymentName"];
+        public static string ModelName => Configuration["AzureOpenAI:ModelName"];
+    }
+}
