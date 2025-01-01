5550d5eeb10ea0cd92e39f8470060846fc8e5eb1
jmcaliece <jmcaliece@mycorpaccess.com>
2025-01-01 08:10:48 -0800

    Multi Model POC Complete

---
+++ MultiModelSolution/MultiModel/Program.cs
@@ -0,0 +1,79 @@
+﻿#pragma warning disable SKEXP0050
+
+using Microsoft.SemanticKernel;
+using MultiModel.Settings;
+
+namespace MultiModel
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Multi-Model Demo");
+            try
+            {
+
+                // Display all Azure Settings to the console.
+                Console.WriteLine($"Endpoint: {Settings.AzureOpenAiSettings.Endpoint}");
+                Console.WriteLine($"ApiKey: {Settings.AzureOpenAiSettings.ApiKey}");
+                Console.WriteLine($"DeploymentNameGPT4o: {Settings.AzureOpenAiSettings.DeploymentNameGPT4o}");
+                Console.WriteLine($"ModelNameGPT4o: {Settings.AzureOpenAiSettings.ModelNameGPT4o}");
+                Console.WriteLine($"DeploymentNameGP35Turbo: {Settings.AzureOpenAiSettings.DeploymentNameGPT35Turbo}");
+                Console.WriteLine($"ModelNameGP35Turbo: {Settings.AzureOpenAiSettings.ModelNameGPT35Turbo}");
+                Console.WriteLine("Press any key to exit.");
+
+                // Build the Kernel
+                var builder = Kernel.CreateBuilder();
+                builder.AddAzureOpenAIChatCompletion(
+                    AzureOpenAiSettings.DeploymentNameGPT4o,
+                    AzureOpenAiSettings.Endpoint,
+                    AzureOpenAiSettings.ApiKey,
+                    AzureOpenAiSettings.ModelNameGPT4o);
+                builder.AddAzureOpenAIChatCompletion(
+                    AzureOpenAiSettings.DeploymentNameGPT35Turbo,
+                    AzureOpenAiSettings.Endpoint,
+                    AzureOpenAiSettings.ApiKey,
+                    AzureOpenAiSettings.ModelNameGPT35Turbo);
+                var kernel = builder.Build();
+
+                // Create a Prompt
+                var prompt = "Did Monty Python sing a song about lumberjacks?";
+
+                // Create KernelArguments, one for each model
+                KernelArguments kernelArguments = new()
+                {
+                    ExecutionSettings = new Dictionary<string, PromptExecutionSettings>
+                {
+                    { AzureOpenAiSettings.ModelNameGPT35Turbo, new() }
+                }
+                };
+
+                var response = await kernel.InvokePromptAsync(prompt, kernelArguments);
+                Console.WriteLine($"Response from GPT-3.5-Turbo: {response}");
+
+                kernelArguments = new()
+                {
+                    ExecutionSettings = new Dictionary<string, PromptExecutionSettings>
+                {
+                    { AzureOpenAiSettings.ModelNameGPT35Turbo, new() }
+                }
+                };
+
+                response = await kernel.InvokePromptAsync(prompt, kernelArguments);
+                Console.WriteLine($"Response from GPT-4o: {response}");
+
+
+
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                // Write the exception to the console.
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ MultiModelSolution/MultiModel/Settings/AzureOpenAiSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+namespace MultiModel.Settings
+{
+    public static class AzureOpenAiSettings
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static AzureOpenAiSettings()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        public static string Endpoint => Configuration["AzureOpenAI:Endpoint"];
+        public static string ApiKey => Configuration["AzureOpenAI:ApiKey"];
+        public static string DeploymentNameGPT4o => Configuration["AzureOpenAI:DeploymentNameGPT4o"];
+        public static string ModelNameGPT4o => Configuration["AzureOpenAI:ModelNameGPT4o"];
+        public static string DeploymentNameGPT35Turbo => Configuration["AzureOpenAI:DeploymentNameGPT35Turbo"];
+        public static string ModelNameGPT35Turbo => Configuration["AzureOpenAI:ModelNameGPT35Turbo"];
+    }
+}
